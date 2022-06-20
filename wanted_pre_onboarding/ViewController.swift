512be53dab839e336dfcd2ba8e97dc92ae1d80f4
seocho <whtjdqls617@gmail.com>
2022-06-20 12:44:04 +0900

    Initial Commit

---
+++ wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by 조성빈 on 2022/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
